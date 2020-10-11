4203adf4620c2a39401ffb974f56632b5c67b9e2
Akshat Gupta <akshatvg23@gmail.com>
2020-10-11 10:55:18 +0530

    working

--- index.js
+++ index.js
@@ -5,28 +5,39 @@ var messages = [],
     botName = 'IPL Bot',
     talking = true;
 
-// Hey
-function chatbotResponse() {
-    talking = true;
-    botMessage = "I didn't get you. Could you please repeat?";
-
-    if (lastUserMessage === 'hi' || lastUserMessage == 'hello') {
-        const hi = ['hi', 'howdy', 'hello']
-        botMessage = hi[Math.floor(Math.random() * (hi.length))];;
-    }
-}
-
 function newEntry() {
     if (document.getElementById("chatbox").value != "") {
         lastUserMessage = document.getElementById("chatbox").value;
         document.getElementById("chatbox").value = "";
         messages.push(lastUserMessage);
-        chatbotResponse();
-        messages.push("<b>" + botName + ":</b> " + botMessage);
-        Speech(botMessage);
-        for (var i = 1; i < 8; i++) {
-            if (messages[messages.length - i])
-                document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
+        var text = {
+            text: lastUserMessage,
+        };
+        var xhr = new XMLHttpRequest();
+        xhr.open(
+            "POST",
+            "http://716234c3c24d.ngrok.io/message",
+            true
+        );
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(text));
+        xhr.onload = function () {
+            if (this.status == 200) {
+                botMessage = this.responseText;
+                messages.push("<b>" + botName + ":</b> " + botMessage);
+                console.log("Success!");
+                Speech(botMessage);
+                for (var i = 1; i < 8; i++) {
+                    if (messages[messages.length - i])
+                        document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
+                }
+            }
+            if (this.status == 400) {
+                console.log("400!");
+            }
+            else {
+                console.log("Error");
+            }
         }
     }
 }
